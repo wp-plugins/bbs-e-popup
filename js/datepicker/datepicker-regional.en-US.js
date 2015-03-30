9dbe9137bc0be0e927b1666ec4b81aba76045d4c
bbsetheme <bbsetheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 03:41:55 +0000

    update 1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/bbs-e-popup/tags/1.5.0@1123316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/datepicker/datepicker-regional.en-US.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function() {
+	jQuery.datepicker.regional['en-US'] = {
+			closeText: 'Done',
+			prevText: 'Prev',
+			nextText: 'Next',
+			currentText: 'Today',
+			monthNames: ['January','February','March','April','May','June',
+			'July','August','September','October','November','December'],
+			monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+			dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+			dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+			weekHeader: 'Wk',
+			dateFormat: 'mm-dd-yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''};
+	jQuery.datepicker.setDefaults(jQuery.datepicker.regional['en-US']);
+
+	jQuery(".datepicker").datepicker(jQuery.datepicker.regional["en-US"]);
+	jQuery('.datepicker').datepicker('option', {dateFormat:'mm-dd-yy'});
+});
